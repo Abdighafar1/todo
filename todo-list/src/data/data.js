3987b39f1368b2d58b0a94d37e4c4774aa7a33fe
Abdighafar1 <abdighafar.ali@student.moringaschool.com>
2024-02-08 08:48:07 +0300

    Done with assignment

---
+++ todo-list/src/data/data.js
@@ -0,0 +1,8 @@
+const todos = [
+    { id: 1, title: 'Brush my teeth'},
+    { id: 2, title: 'Eat breakfast'},
+    { id: 3, title: 'Take a shower'},
+    { id: 4, title: 'Exercise'},
+]
+
+export default todos;
